c3c56dcd7c1d318a0ad1b36eaeffb2145386dd67
Jeff Hammond <hammondjeffrey@gmail.com>
2015-10-18 16:56:12 -0600

    Initial Commit

---
+++ Project0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project0
+//
+//  Created by Jeffrey on 10/18/15.
+//  Copyright (c) 2015 Jeffrey Hammond. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
